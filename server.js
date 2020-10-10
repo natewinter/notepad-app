5936c9311832fe24d2f39fb173bfedf459876f3f
Josh Brown <joshbrown2794@gmail.com>
2020-10-09 17:09:05 -0700

    adding / route

--- server.js
+++ server.js
@@ -3,31 +3,36 @@ const path = require("path");
 const express = require("express");
 const fs = require("fs")
 var uniqid = require('uniqid');
- 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(express.static("public"))
 
 //GET ROUTE FOR db.json DATA
-function getDatabase(){
-    let dbData = fs.readFileSync(path.join(__dirname,"/db/db.json"),"utf8");
+function getDatabase() {
+    let dbData = fs.readFileSync(path.join(__dirname, "/db/db.json"), "utf8");
     dbData = JSON.parse(dbData);
     let noteData = dbData
     // console.log(noteData)
     return noteData
 }
 
-function postDatabase(newData){
-    fs.writeFileSync(path.join(__dirname,"/db/db.json"),JSON.stringify(newData),function(err,data){
-        if(err)throw err;;
-    })}
+function postDatabase(newData) {
+    fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(newData), function (err, data) {
+        if (err) throw err;;
+    })
+}
 
 
-
+app.get("/", function (req, res) {
+    res.json(path.join(__dirname, "/public/index.html"));
+});
 
 app.get("/index", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -41,19 +46,26 @@ app.get("/api/notes", function (req, res) {
     res.json(getDatabase())
 })
 
-app.post("/api/notes", function(req, res) {
-    var data = getDatabase()||[]
-    var{title, text} = req.body
-    var newNote = {title, text, id:uniqid()}
+app.post("/api/notes", function (req, res) {
+    var data = getDatabase() || []
+    var {
+        title,
+        text
+    } = req.body
+    var newNote = {
+        title,
+        text,
+        id: uniqid()
+    }
     data.push(newNote)
     postDatabase(data)
     res.json(data)
 });
 
-app.delete("/api/notes/:id",function(req,res){
-    var data = getDatabase()||[]
+app.delete("/api/notes/:id", function (req, res) {
+    var data = getDatabase() || []
     data = data.filter(note => note.id !== req.params.id)
-    postDatabase(data) 
+    postDatabase(data)
     res.json(data)
 });
 
