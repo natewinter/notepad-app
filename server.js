3db9f8241b306a41627efcba108eebd508d3d4c3
Josh Brown <joshbrown2794@gmail.com>
2020-10-09 20:08:28 -0700

    pulling

--- server.js
+++ server.js
@@ -73,8 +73,6 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
-
 app.listen(PORT, function () {
     console.log("listening on port " + PORT)
 })
